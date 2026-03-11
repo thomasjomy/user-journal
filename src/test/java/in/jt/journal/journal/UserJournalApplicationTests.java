3244c151122945f5a7db4d67cbfe9013a7d90e8b
Jomy Thomas <jomythomas001@gmail.com>
2026-03-11 13:50:19 +0530

    project setup

---
+++ src/test/java/in/jt/journal/journal/UserJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package in.jt.journal.journal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
